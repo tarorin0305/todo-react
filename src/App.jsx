42d5e6aefa329dc7eddedfa9b730545ac7ab3ddf
zakky / Rintaro Yamazaki <dayutakatarorin@gmail.com>
2021-04-23 10:27:38 +0900

    コンポーネント化を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import { useState } from "react";
 import "./styles.css";
 import { InputTodo } from "./components/inputTodo";
+import { IncompleteTodos } from "./components/incompleteTodos";
 
 export default function App() {
   const [todoText, setTodoText] = useState("");
-  const [incompletTodos, setIncompleteTodos] = useState([]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
   const [completeTodos, setCompleteTodos] = useState([]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
   const onClickAdd = () => {
     if (todoText === "") return;
-    const newTodos = [...incompletTodos, todoText];
+    const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     setTodoText("");
   };
   const onClickDelete = (index) => {
-    const newTodos = [...incompletTodos];
+    const newTodos = [...incompleteTodos];
     newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
   };
   const onClickComplete = (index) => {
-    const newIncompleteTodos = [...incompletTodos];
+    const newIncompleteTodos = [...incompleteTodos];
     newIncompleteTodos.splice(index, 1);
 
-    const newCompleteTodos = [...completeTodos, incompletTodos[index]];
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
     setIncompleteTodos(newIncompleteTodos);
     setCompleteTodos(newCompleteTodos);
   };
@@ -31,7 +32,7 @@ export default function App() {
     const newCompleteTodos = [...completeTodos];
     newCompleteTodos.splice(index, 1);
 
-    const newIncompleteTodos = [...incompletTodos, completeTodos[index]];
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
     setCompleteTodos(newCompleteTodos);
     setIncompleteTodos(newIncompleteTodos);
   };
@@ -42,20 +43,12 @@ export default function App() {
         onChange={onChangeTodoText}
         onClick={onClickAdd}
       />
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompletTodos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
